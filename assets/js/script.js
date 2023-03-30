53354bb15fe0bc16d3568315764c4d25e914f6b9
eschindev <118208710+eschindev@users.noreply.github.com>
2023-03-29 18:51:16 -0700

    added validation for character selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,46 @@
-// Assignment code here
-var lowers = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+// Define the arrays of character types and the necessary variables
+var lowers = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+var uppers = 'QWERTYUIOPASDFGHJKLZXCVBNM'.split('');
 var nums = '1234567890'.split('');
 var specs = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
   ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 var passLength = 0;
 var validLength = false;
+var validChars = false;
 var passChars = [];
 
-function generatePassword () {
+// generate password by asking for desired length (and validating), confirming each character type, adding relevant character type arrays to the password generation array, and then calling the genFromArray() function
+function generatePassword() {
+  passLength = 0;
   validLength = false;
+  passChars = [];
   do {
     passLength = prompt("How long would you like your password to be? (8-128)");
     if (!Number.isInteger(numChars) || numChars < 8 || numChars > 128) {
       alert("Password must be a number from 8 to 128.");
     } else validLength = true;
   } while (!validLength);
-  if (confirm("Would you like your password to include lowercase letters?")) {
-    passChars.concat(lowers);
-  }
-  if (confirm("Would you like your password to include uppercase letters?")) {
-    passChars.concat(uppers);
-  }
-  if (confirm("Would you like your password to include numbers?")) {
-    passChars.concat(nums);
-  }
-  if (confirm("Would you like your password to include lowercase letters?")) {
-    passChars.concat(specs);
-  }
+  do {
+    if (confirm("Would you like your password to include lowercase letters?")) {
+      passChars.concat(lowers);
+      validChars = true;
+    }
+    if (confirm("Would you like your password to include uppercase letters?")) {
+      passChars.concat(uppers);
+      validChars = true;
+    }
+    if (confirm("Would you like your password to include numbers?")) {
+      passChars.concat(nums);
+      validChars = true;
+    }
+    if (confirm("Would you like your password to include lowercase letters?")) {
+      passChars.concat(specs);
+      validChars = true;
+    }
+    if (!validChars) {
+      alert("You must select at least one character type to include in your password.")
+    }
+  } while (!validChars);
   return genFromArray(passChars);
 }
 
